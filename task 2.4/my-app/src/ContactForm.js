29cd3753c20a6bc02ed60eac03ae0ae6e28ed554
LumWenKang <lumlaoren@gmail.com>
2023-04-05 00:43:32 +0800

    updated form

--- task 2.4/my-app/src/ContactForm.js
+++ task 2.4/my-app/src/ContactForm.js
@@ -1,14 +1,36 @@
 import React, { useState } from 'react';
 
-function ContactForm({ addContact }) {
+function ContactForm() {
     const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [gender, setGender] = useState('');
     const [phone, setPhone] = useState('');
 
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
         event.preventDefault();
-        addContact({ name, phone });
-        setName('');
-        setPhone('');
+        const formData = {
+            name: name,
+            email: email,
+            gender: gender,
+            phone: phone
+        };
+        try {
+            const response = await fetch('/api/contacts', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formData)
+            });
+            if (response.ok) {
+                alert('Contact saved successfully');
+            } else {
+                alert('Failed to save contact');
+            }
+        } catch (error) {
+            console.error(error);
+            alert('Failed to save contact');
+        }
     };
 
     return (
@@ -17,13 +39,23 @@ function ContactForm({ addContact }) {
                 Name:
                 <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
             </label>
-            <br />
+            <label>
+                Email:
+                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            </label>
+            <label>
+                Gender:
+                <select value={gender} onChange={(e) => setGender(e.target.value)}>
+                    <option value="">--Select Gender--</option>
+                    <option value="Male">Male</option>
+                    <option value="Female">Female</option>
+                </select>
+            </label>
             <label>
                 Phone:
                 <input type="text" value={phone} onChange={(e) => setPhone(e.target.value)} />
             </label>
-            <br />
-            <button type="submit">Add Contact</button>
+            <button type="submit">Submit</button>
         </form>
     );
 }
