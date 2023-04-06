94191b0eb35e6b5e6c049c717461cbe404a34b5b
LumWenKang <lumlaoren@gmail.com>
2023-04-06 11:45:12 +0800

    added routing for authentication and authorization

---
+++ task 3/api-routes.js
@@ -0,0 +1,34 @@
+// Initialize express router
+let router = require('express').Router();
+const authController = require('./authController');
+
+// Set default API response
+router.get('/', function (req, res) {
+    res.json({
+        status: 'API Its Working',
+        message: 'Welcome to RESTHub!',
+    });
+});
+
+// Import contact controller
+const contactController = require('./contactController');
+
+// Import authentication middleware
+const { authenticateUser } = require('./authController');
+
+// Contact routes
+router.route('/contacts')
+    .get(authenticateUser, contactController.index)
+    .post(authenticateUser, contactController.new);
+
+router.route('/contacts/:contact_id')
+    .get(authenticateUser, contactController.view)
+    .patch(authenticateUser, contactController.update)
+    .put(authenticateUser, contactController.update)
+    .delete(authenticateUser, contactController.delete);
+
+// Define the login route
+router.post('/login', authController.login);
+
+// Export API routes
+module.exports = router;
